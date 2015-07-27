41b0348940df0ca6d96e4e7ac660deb0b1473e14
C. Bryan Daniels <cdaniels@nandor.net>
2015-07-27 00:23:42 -0500

    fixed README.md gkno.sh

--- gkno.sh
+++ gkno.sh
@@ -3,9 +3,9 @@
 # gkno - Genomics Package
 # 7/26/15
 
-sudo apt-get install git ant cmake gcc g++ make openjdk-7-jdk ruby
+sudo /usr/bin apt-get install git ant cmake gcc g++ make openjdk-7-jdk ruby
 
-sudo apt-get build-dep samtools valgrind bwa samtools
+sudo /usr/bin/ apt-get build-dep samtools valgrind bwa samtools
 
 git clone https://github.com/gkno/gkno_launcher.git
 
